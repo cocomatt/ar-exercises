e0b5055c563a67783df9558f9f6e1857a61b1e2d
cocomatt <matt.currie@outlook.com>
2018-03-01 03:19:07 +0000

    completed exercise 7 (I think); this will need to be checked

--- exercises/exercise_7.rb
+++ exercises/exercise_7.rb
@@ -9,4 +9,10 @@ require_relative './exercise_6'
 puts "Exercise 7"
 puts "----------"
 
-# Your code goes here ...
+puts "Please type a store name and press 'enter':"
+print "> "
+@user_entered_store = gets.chomp
+
+new_store = Store.new(name: user_entered_store)
+new_store.valid?
+puts new_store.errors.full_messages
